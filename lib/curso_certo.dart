a252fcb059cf7b71078367745f6fe91585df9f1f
Renato Leal <marcondesleal@icloud.com>
2022-02-25 11:10:05 -0300

    Criação dos primeiros arquivos
    Criação do Widget CursoCerto (tela principal) e Details (página de detalhes dos cursos)

---
+++ lib/curso_certo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CursoCerto extends StatelessWidget {
+  const CursoCerto({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Olá mundo!"),
+      ),
+    );
+  }
+}
